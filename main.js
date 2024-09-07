2b2f17f54adeb523b08126135c7e8e6d7ac7a77e
Imad Asserne <imad.asserne@gmail.com>
2024-09-07 21:07:30 +0100

    add swiper to section

--- main.js
+++ main.js
@@ -39,3 +39,32 @@ $(document).ready(function () {
 
     });
 });
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disbleOnInteraction: false
+    },
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});
